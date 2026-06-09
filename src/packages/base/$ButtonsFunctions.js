9b57e48d8f56a22ff69792e935926e16b7861957
Marc Cawood <498834+cawoodm@users.noreply.github.com>
2026-06-09 16:07:31 +0200

    Fix example button

--- src/packages/base/$ButtonsFunctions.js
+++ src/packages/base/$ButtonsFunctions.js
@@ -2,7 +2,7 @@
   * Buttons to Show/Hide Tiddlers:
   * Examples:
   *  - Show all tiddlers
-  *    <<button Click Me!;ui.open.all>>
+  *    <<button "Click Me!" ui.open.all>>
   */
 tw.macros.button = (title, msg, payload = '', id = '') => {
   return tw.ui.button(title, msg, payload, id);
